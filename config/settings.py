1da7ff573b355d5e748e4ec4d66e259c8fe2103c
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-16 18:12:02 +0100

    feat: add django structure

---
+++ config/settings.py
@@ -0,0 +1,119 @@
+import os
+from configparser import ConfigParser
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Configparser reading for getting .env variabesl.
+if not os.path.exists(os.path.join(BASE_DIR, 'settings.ini')):
+    raise FileNotFoundError('Configuration file "settings.ini" not found.')
+
+config = ConfigParser()
+config.read('settings.ini')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config.get('DJANGO', 'SECRET')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config.getboolean('DJANGO', 'DEBUG')
+
+ALLOWED_HOSTS = config.get('DJANGO', 'HOSTS').split(',')
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+} if DEBUG else {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.get('PRODUCTION_DATABASE', 'POSTGRES_NAME'),
+        'HOST': config.get('PRODUCTION_DATABASE', 'POSTGRES_HOST'),
+        'PORT': config.get('PRODUCTION_DATABASE', 'POSTGRES_PORT'),
+        'USER': config.get('PRODUCTION_DATABASE', 'POSTGRES_USER'),
+        'PASSWORD': config.get('PRODUCTION_DATABASE', 'POSTGRES_PASS')
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.%s' % validator}
+    for validator in [
+        'UserAttributeSimilarityValidator',
+        'MinimumLengthValidator',
+        'CommonPasswordValidator',
+        'NumericPasswordValidator',
+    ]
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = config.get('DJANGO', 'LANGUAGE_CODE')
+TIME_ZONE = config.get('DJANGO', 'TIME_ZONE')
+USE_I18N = config.getboolean('DJANGO', 'USE_I18N')
+USE_TZ = config.getboolean('DJANGO', 'USE_TZ')
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
