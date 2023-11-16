44c038f0af0fc4c595121019e548ba60c85bac28
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-17 00:01:19 +0100

    feat: add static files config

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,7 +3,6 @@ from configparser import ConfigParser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-print(BASE_DIR)
 
 # Configparser reading for getting .env variabesl.
 if not os.path.exists(os.path.join(BASE_DIR, 'settings.ini')):
@@ -117,6 +116,11 @@ USE_TZ = config.getboolean('DJANGO', 'USE_TZ')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if DEBUG:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
