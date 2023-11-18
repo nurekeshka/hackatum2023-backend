87eb35b6355c62f3d32656f83829a90d64f5a8c0
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 07:13:35 +0100

    feat: add process, action, recipe admins

--- app/apps/recipes/admin.py
+++ app/apps/recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from apps.recipes import models
 
-# Register your models here.
+
+@admin.register(models.Process)
+class ProcessAdmin(admin.ModelAdmin):
+    fields = ('name', 'image')
+    list_display = ('name', 'image')
+
+
+@admin.register(models.Action)
+class ActionAdmin(admin.ModelAdmin):
+    fields = ('ingredients', 'process', 'outcome')
+    list_display = ('process', 'outcome')
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ('name', 'actions')
+    list_display = ('name',)
