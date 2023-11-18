82dfc7a70ca75c052a0fa69c39b61b2d2a7442b3
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 07:13:16 +0100

    feat: add category, preference, ingredient admins

--- app/apps/components/admin.py
+++ app/apps/components/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from apps.components import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+
+
+@admin.register(models.Preference)
+class PreferenceAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_display = ('name',)
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ('name', ('categories', 'preferences'), 'instance_of', 'image')
+    list_display = ('name', 'instance_of', 'image')
