815b79e663536b8a7b572484822e49682b1af8e2
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 18:45:21 +0100

    feat: add conversion constants

---
+++ app/apps/components/constants.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class FluidConversions(Enum):
+    to_litres = 1000
+    to_tablespoon = 1 / 14.7868
+
+
+class MassConversions(Enum):
+    to_kilograms = 1000
+    to_tablespoon = 1 / 14.175
