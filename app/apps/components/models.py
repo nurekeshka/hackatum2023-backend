c996ce462e0b9a0fa90a705f6cee0827052eeb4e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 06:38:17 +0100

    feat: add ingredient model fields

--- app/apps/components/models.py
+++ app/apps/components/models.py
@@ -1,11 +1,5 @@
+from apps.core.models import Image
 from django.db import models
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=255, verbose_name='name')
-
-    def __str__(self) -> str:
-        return str(self.name)
 
 
 class Category(models.Model):
@@ -17,6 +11,27 @@ class Category(models.Model):
 
 class Preference(models.Model):
     name = models.CharField(max_length=255, verbose_name='name')
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, verbose_name='name')
+    categories = models.ManyToManyField(Category, verbose_name='categories')
+    preferences = models.ManyToManyField(Preference, verbose_name='preference')
+
+    instance_of = models.ForeignKey(
+        'Ingredient', on_delete=models.CASCADE, verbose_name='instance of')
+
+    image = models.ForeignKey(
+        Image, on_delete=models.SET_NULL,
+        null=True, blank=True,
+        verbose_name='image')
+
+    class Meta:
+        verbose_name = 'ingredient'
+        verbose_name_plural = 'ingredients'
 
     def __str__(self) -> str:
         return str(self.name)
