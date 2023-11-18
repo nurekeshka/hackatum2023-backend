6e09598d887a95be8f2dee8dac9a326c7c8606a1
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 18:46:43 +0100

    feat: add to cup conversion

--- app/apps/components/constants.py
+++ app/apps/components/constants.py
@@ -4,8 +4,10 @@ from enum import Enum
 class FluidConversions(Enum):
     to_litres = 1000
     to_tablespoon = 1 / 14.7868
+    to_cup = 1 / 240
 
 
 class MassConversions(Enum):
     to_kilograms = 1000
     to_tablespoon = 1 / 14.175
+    to_cup = 1 / 240
