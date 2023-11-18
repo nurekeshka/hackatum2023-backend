9afd8abf31eb1bc3e8070472c89b436cf3ec5562
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 06:31:08 +0100

    feat: add ingredient model

--- app/apps/components/models.py
+++ app/apps/components/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, verbose_name='name')
+
+    def __str__(self) -> str:
+        return str(self.name)
