7b0d3f363a0076ed97424b971d39c034741c0e88
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 06:35:56 +0100

    feat: add image model

--- app/apps/core/models.py
+++ app/apps/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    link = models.URLField(verbose_name='link')
+
+    def __str__(self) -> str:
+        return str(self.link)
+
+    class Meta:
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
