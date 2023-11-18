4b488a2cb7ed1f93d34e1372487c5a0042474525
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 18:04:32 +0100

    feat: add parameter, attribute, step admins

--- app/apps/recipes/admin.py
+++ app/apps/recipes/admin.py
@@ -2,19 +2,37 @@ from django.contrib import admin
 from apps.recipes import models
 
 
+@admin.register(models.Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    fields = ('name', 'unit')
+    list_display = ('name', 'unit')
+
+
 @admin.register(models.Process)
 class ProcessAdmin(admin.ModelAdmin):
-    fields = ('name', 'image')
+    fields = ('name', 'parameters', 'image')
     list_display = ('name', 'image')
+
+
+@admin.register(models.Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    fields = ('parameter', 'value')
+    list_display = ('parameter', 'value')
 
 
 @admin.register(models.Action)
 class ActionAdmin(admin.ModelAdmin):
-    fields = ('ingredients', 'process', 'outcome')
-    list_display = ('process', 'outcome')
+    fields = ('ingredients', 'process', 'attributes', 'product')
+    list_display = ('process', 'product')
+
+
+@admin.register(models.Step)
+class StepAdmin(admin.ModelAdmin):
+    fields = ('number', 'action')
+    list_display = ('number', 'action')
 
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    fields = ('name', 'actions')
+    fields = ('name', 'steps')
     list_display = ('name',)
