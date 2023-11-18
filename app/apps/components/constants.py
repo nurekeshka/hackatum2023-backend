412b67d96dc72a8e67c2cea3467c07c7950feea5
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-18 18:47:42 +0100

    feat: add conversion to teaspoons

--- app/apps/components/constants.py
+++ app/apps/components/constants.py
@@ -4,10 +4,12 @@ from enum import Enum
 class FluidConversions(Enum):
     to_litres = 1000
     to_tablespoon = 1 / 14.7868
+    to_teaspoon = 1 / 5
     to_cup = 1 / 240
 
 
 class MassConversions(Enum):
     to_kilograms = 1000
     to_tablespoon = 1 / 14.175
+    to_teaspoon = 1 / 5
     to_cup = 1 / 240
